a3b5af19d54f265c47541c7bb0b0b67f88b74aee
29584 <2958474980@qq.com>
2024-10-20 18:11:30 +0800

    优化更新

--- app/src/main/java/com/example/brimonotification/service/NotionalPoolingAccessibilityService.java
+++ app/src/main/java/com/example/brimonotification/service/NotionalPoolingAccessibilityService.java
@@ -6,9 +6,6 @@ import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
-import com.example.brimonotification.utils.AccessibilityUtils;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +14,8 @@ import java.util.List;
 public class NotionalPoolingAccessibilityService extends AccessibilityService {
     //2024-10-20 08:41:43.532 20051-20051 控件信息                    com.example.brimonotification        D  android.view.accessibility.AccessibilityNodeInfo@5f4ff; boundsInParent: Rect(0, 0 - 656, 96); boundsInScreen: Rect(32, 1261 - 688, 1357); boundsInWindow: Rect(32, 1261 - 688, 1357); packageName: id.co.bri.brimo; className: android.widget.EditText; text: Password; error: null; maxTextLength: -1; stateDescription: null; contentDescription: null; tooltipText: null; containerTitle: null; viewIdResName: id.co.bri.brimo:id/et_password; uniqueId: null; checkable: false; checked: false; focusable: true; focused: false; selected: false; clickable: true; longClickable: true; contextClickable: false; enabled: true; password: true; scrollable: false; importantForAccessibility: true; visible: true; actions: [AccessibilityAction: ACTION_FOCUS - null, AccessibilityAction: ACTION_SELECT - null, AccessibilityAction: ACTION_CLEAR_SELECTION - null, AccessibilityAction: ACTION_CLICK - null, AccessibilityAction: ACTION_LONG_CLICK - null, AccessibilityAction: ACTION_ACCESSIBILITY_FOCUS - null, AccessibilityAction: ACTION_SET_TEXT - null, AccessibilityAction: ACTION_SHOW_ON_SCREEN - null]; isTextSelectable: false
     //2024-10-20 08:41:43.535 20051-20051 控件信息                    com.example.brimonotification        D  android.view.accessibility.AccessibilityNodeInfo@616c8; boundsInParent: Rect(0, 0 - 656, 96); boundsInScreen: Rect(32, 1405 - 688, 1501); boundsInWindow: Rect(32, 1405 - 688, 1501); packageName: id.co.bri.brimo; className: android.widget.Button; text: Login; error: null; maxTextLength: -1; stateDescription: null; contentDescription: null; tooltipText: null; containerTitle: null; viewIdResName: id.co.bri.brimo:id/button_login; uniqueId: null; checkable: false; checked: false; focusable: true; focused: false; selected: false; clickable: true; longClickable: false; contextClickable: false; enabled: false; password: false; scrollable: false; importantForAccessibility: true; visible: true; actions: [AccessibilityAction: ACTION_FOCUS - null, AccessibilityAction: ACTION_SELECT - null, AccessibilityAction: ACTION_CLEAR_SELECTION - null, AccessibilityAction: ACTION_ACCESSIBILITY_FOCUS - null, AccessibilityAction: ACTION_NEXT_AT_MOVEMENT_GRANULARITY - null, AccessibilityAction: ACTION_PREVIOUS_AT_MOVEMENT_GRANULARITY - null, AccessibilityAction: ACTION_SET_SELECTION - null, AccessibilityAction: ACTION_SHOW_ON_SCREEN - null]; isTextSelectable: false
+    //id.co.bri.brimo:id/btn_login
+
     private static final String TAG = "NotionalPoolingAccessibilityService";
     private final String pass = "Coc135689";
 
@@ -28,19 +27,10 @@ public class NotionalPoolingAccessibilityService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        handlerData(event.getSource());
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
         if (nodeInfo == null) return;
         handleLogin(nodeInfo);
-    }
 
-    private void handlerData(AccessibilityNodeInfo nodeInfo) {
-        if (nodeInfo == null) return;
-        List<AccessibilityNodeInfo> nodeInfos = new ArrayList<>();
-        AccessibilityUtils.getAccessibilityNodeInfoS(nodeInfos, nodeInfo);
-        for (AccessibilityNodeInfo nodeInfo1 : nodeInfos) {
-            Log.d("控件信息", nodeInfo1.toString());
-        }
     }
 
 
@@ -68,23 +58,15 @@ public class NotionalPoolingAccessibilityService extends AccessibilityService {
      * @param nodeInfo
      * @param id
      * @param text
-     * @return 如果已经输入，则不需要数据
      */
-    private boolean EditNodeInfo(AccessibilityNodeInfo nodeInfo, String id, String text) {
+    private void EditNodeInfo(AccessibilityNodeInfo nodeInfo, String id, String text) {
         List<AccessibilityNodeInfo> Passwords = nodeInfo.findAccessibilityNodeInfosByViewId(id);
         for (AccessibilityNodeInfo pass : Passwords) {
-            Log.d("输入控件信息", pass.toString());
-            if (pass == null) continue;
-            editText(pass, text);
             String string = pass.getText().toString();
             if (!string.equals(text)) {
                 editText(pass, text);
-            } else {
-                Log.d("已经输入", string);
-                return true;
             }
         }
-        return false;
     }
 
     /**
@@ -96,7 +78,7 @@ public class NotionalPoolingAccessibilityService extends AccessibilityService {
     private void ClickNodeInfo(AccessibilityNodeInfo nodeInfo, String id) {
         List<AccessibilityNodeInfo> logins = nodeInfo.findAccessibilityNodeInfosByViewId(id);
         for (AccessibilityNodeInfo login : logins) {
-            Log.d("点击", login.toString());
+            Log.d("显示状态", String.valueOf(login.isVisibleToUser()));
             login.performAction(AccessibilityNodeInfo.ACTION_CLICK);
         }
     }
