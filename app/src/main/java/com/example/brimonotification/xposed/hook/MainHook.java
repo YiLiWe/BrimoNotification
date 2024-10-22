3731208b3a146bd4b1f8900eaa64aeab16a386a1
29584 <2958474980@qq.com>
2024-10-23 07:48:54 +0800

    测试cc

--- app/src/main/java/com/example/brimonotification/xposed/hook/MainHook.java
+++ app/src/main/java/com/example/brimonotification/xposed/hook/MainHook.java
@@ -2,11 +2,19 @@ package com.example.brimonotification.xposed.hook;
 
 import com.example.brimonotification.xposed.utils.Log;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage {
+    private static int modify = 0;
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (loadPackageParam.packageName.equals("id.co.bri.brimo")) {
@@ -33,6 +41,45 @@ public class MainHook implements IXposedHookLoadPackage {
                     }
 
                     super.beforeHookedMethod(param);
+                }
+            });
+            // 定义全局变量 modify
+            XposedHelpers.findAndHookMethod(Method.class, "getModifiers", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    Method method = (Method) param.thisObject;
+                    String[] array = new String[]{"getDeviceId"};
+                    String method_name = method.getName();
+                    if (Arrays.asList(array).contains(method_name)) {
+                        modify = 0;
+                    } else {
+                        modify = (int) param.getResult();
+                    }
+
+                    super.afterHookedMethod(param);
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(Modifier.class, "isNative", int.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    param.args[0] = modify;
+
+                    super.beforeHookedMethod(param);
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(BufferedReader.class, "readLine", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    String result = (String) param.getResult();
+                    if(result != null) {
+                        if (result.contains("/data/data/de.robv.android.xposed.installer/bin/XposedBridge.jar")) {
+                            param.setResult("");new File("").lastModified();
+                        }
+                    }
+
+                    super.afterHookedMethod(param);
                 }
             });
             ApplicationHook.getInstance(loadPackageParam);
