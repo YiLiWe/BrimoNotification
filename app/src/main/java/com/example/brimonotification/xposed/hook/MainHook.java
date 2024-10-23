7c9255d925c8b3d6a39544e4ab7aa02983c3c390
29584 <2958474980@qq.com>
2024-10-23 09:38:28 +0800

    cs

--- app/src/main/java/com/example/brimonotification/xposed/hook/MainHook.java
+++ app/src/main/java/com/example/brimonotification/xposed/hook/MainHook.java
@@ -1,28 +1,21 @@
 package com.example.brimonotification.xposed.hook;
 
-import com.example.brimonotification.xposed.utils.Log;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
         if (loadPackageParam.packageName.equals("id.co.bri.brimo")) {
-            ApplicationHook.getInstance(loadPackageParam);
-            ActivityHook.getInstance(loadPackageParam).startActivity();
+            EzXHelperInit.INSTANCE.initHandleLoadPackage(loadPackageParam);
         }
     }
 
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
+        EzXHelperInit.INSTANCE.initZygote(startupParam);
     }
 }
